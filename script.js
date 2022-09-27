fed505e92cfbff4029a076b366659c02e092ce4d
NATH <itynath@yahoo.com>
2022-09-27 06:50:56 +0100

    javascript-basics

---
+++ script.js
@@ -0,0 +1,3 @@
+// this is sign is use to write comment in javascript, means this line is ignored from the code//
+/* also this sign*/
+ //sentence is a line of code and it ends with ; in most language althought javascript has some exceptions// 
